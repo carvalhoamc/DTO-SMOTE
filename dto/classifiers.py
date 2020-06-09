4ab7b0d563a249c16966ffd360213b561254e48e
Alexandre Miguel de Carvalho <professorcarvalho@roboticando.com>
2020-06-09 10:38:20 -0300

    Update classifiers.py

--- dto/classifiers.py
+++ dto/classifiers.py
@@ -1,9 +1,6 @@
-#from imblearn.ensemble import BalancedBaggingClassifier, EasyEnsembleClassifier, RUSBoostClassifier
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression, SGDClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
@@ -21,4 +18,4 @@ classifiers = {"RF": RandomForestClassifier(n_estimators=100),
 			   "SGD": SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
 			   }
 
-classifiers_list = ['RF','KNN','DTREE','GNB','LRG','ABC','MLP','QDA','SVM','SGD']
+classifiers_list = ['RF','KNN','DTREE','LRG','ABC','MLP','SVM','SGD']
